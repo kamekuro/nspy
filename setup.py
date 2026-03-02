2c6c25f3cc99e10a9157f03782df54033b335d2c
Vladcom4iiik <solovyev4625@gmail.com>
2026-03-02 22:39:56 +0000

    release v3.3.1
    • 0e84e03 Add Volgograd region and update count

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="netschoolpy",
-    version="3.3.0",
+    version="3.3.1",
     description="Асинхронный клиент для «Сетевого города»",
     long_description=long_description,
     long_description_content_type="text/markdown",
