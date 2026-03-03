3537a62214461d230855c459f6f5384d72ca8294
Vladcom4iiik <solovyev4625@gmail.com>
2026-03-03 07:37:51 +0000

    release v3.3.3
    • a051624 fix: add proxy param to search_schools standalone function
    • d2f8dc3 feat: добавлено поддержка прокси для HTTP-запросов и обновлён справочник регионов
    • fa1f6d3 Add Volgograd region to REGIONS
    • 2186c56 Merge branch 'main' of https://github.com/Vladcom4iiik/netschoolpy

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="netschoolpy",
-    version="3.3.2",
+    version="3.3.3",
     description="Асинхронный клиент для «Сетевого города»",
     long_description=long_description,
     long_description_content_type="text/markdown",
